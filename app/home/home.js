c14269e284f87275129ac0ce7240124d2ee7edcc
Nick Balkissoon <nickbalkissoon@Nicks-MacBook-Pro.local>
2015-06-17 19:26:00 -0700

    basic creating of posts and realtime updating of list via Firebase

--- app/home/home.js
+++ app/home/home.js
@@ -18,18 +18,44 @@ deedsAppHomeModule.config(['$routeProvider', function($routeProvider) {
  /* Controller */
 
 deedsAppHomeModule.controller('HomeCtrl', ['$scope', '$location', function($scope, $location) {
+  
+  //Variables
   var ref = new Firebase("https://burning-inferno-9477.firebaseio.com/");
   var authData = ref.getAuth();
+  $scope.newPost="New Post";
+
+  //Redirect to login if not logged in
   if(!authData){
     $location.path('/login');
   }
-  
-  $scope.post="New Post";
-  //Retrieve JSON formatted posts for specific user from Firebase
+
+  //Create reference to user's posts
+  var usersRef=ref.child("users");
+  var userPostsRef=usersRef.child(authData.uid);
+
+
+  //Realtime query user's posts
+  userPostsRef.on("value", function(snapshot) {
+    console.log(snapshot.val());
+    $scope.userPosts=snapshot.val();
+  }, 
+    function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    }
+  );
+
+  //Logout Function
   $scope.logout=function(event){
     ref.unauth();
     $location.path('/login');
     console.log("Succesfully Logged Out User: "+authData.uid);
+  }
+
+  //Submit Function
+  $scope.submitPost=function(event){
+
+    userPostsRef.push($scope.newPost);
+    console.log("Succesfully submitted post: "+$scope.newPost);
   }
 
 }]);
